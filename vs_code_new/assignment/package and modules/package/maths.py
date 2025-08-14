89c0655e3ff7de1cbd05bfa9eb79dda766882688
Karan Singh Negi <karan.sn2004@gmail.com>
2025-08-14 17:23:11 +0530

    Add the new folder VS_code_new which contain some old python basic

---
+++ vs_code_new/assignment/package and modules/package/maths.py
@@ -0,0 +1,29 @@
+def addition(a, b):
+    """Returns the sum of a and b."""
+    return a + b
+
+
+def subtraction(a, b):
+    """Returns the difference of a and b."""
+    return a - b
+def multiplication(a, b):
+    """Returns the product of a and b."""
+    return a * b
+def division(a, b):
+    """Returns the quotient of a and b."""
+    return a / b      
+def exponentiation(base, exponent):
+    """Returns base raised to the power of exponent."""
+    return base ** exponent
+def modulus(a, b):
+    """Returns the remainder of a divided by b."""
+    return a % b
+def floor_division(a, b):
+    """Returns the floor of the quotient of a and b."""
+    return a // b
+def absolute_value(a):
+    """Returns the absolute value of a."""
+    return abs(a)
+def round_value(a):
+    """Returns the rounded value of a."""
+    return round(a)
---
+++ vs_code_new/assignment/package and modules/package/test.py
@@ -0,0 +1,4 @@
+from package.maths import*
+from package.subpackage.mul import mul as multiplication
+print(addition(5, 3))
+print(multiplication(5, 3))
